64f31cb61f130cc436b6f37c2bd27a00b4d4f6c5
chucksupasi <chuck.supasi@gmail.com>
2026-03-16 19:15:09 +0800

    Add ability to mark and unmark tasks as done

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -5,6 +5,7 @@ public class Echo {
         Scanner sc = new Scanner(System.in);
         String[] tasks = new String[100];
         int taskCount = 0;
+        boolean[] done = new boolean[100];
 
         System.out.println("Hey there! I'm Echo, your personal assistant");
         System.out.println("What can I do for you?");
@@ -16,12 +17,27 @@ public class Echo {
                 break;
             }
             else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = done[i] ? "[X]" : "[ ]";
+                    System.out.println((i + 1) + ". " + status + " " + tasks[i]);
                 }
+            }
+            else if (input.startsWith("mark ")) {
+                int num = Integer.parseInt(input.substring(5)) - 1;
+                done[num] = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + tasks[num]);
+            }
+            else if (input.startsWith("unmark ")) { // ADDED: mark task as not done
+                int num = Integer.parseInt(input.substring(7)) - 1;
+                done[num] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + tasks[num]);
             }
             else {
                 tasks[taskCount] = input;
+                done[taskCount] = false;
                 taskCount++;
                 System.out.println("added: " + input);
             }
