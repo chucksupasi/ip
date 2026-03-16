f02c4a0d17bcf29ada1368b3c8ca3ce3d09f00f0
chucksupasi <chuck.supasi@gmail.com>
2026-03-16 18:08:39 +0800

    Add ability to store and print stored text

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Echo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("Hey there! I'm Echo, your personal assistant");
         System.out.println("What can I do for you?");
@@ -13,7 +15,16 @@ public class Echo {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(input);
+            else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            }
+            else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+            }
         }
         sc.close();
     }
